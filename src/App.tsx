53c496317b8600aeb84a093a89a5ad595cace5e4
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-11-04 18:09:30 +0200

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import ImageModal from "./components/ImageModal/ImageModal";
-import Loader from "./components/Loader/Loader";
-import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
-import SearchBar from "./components/SearchBar/SearchBar";
 import { useEffect, useState } from "react";
 import { fetchArticles } from "./components/services/api";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
   type Url = {
